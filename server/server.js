044f6506108f5f71c9450cba23e3b83704fd7517
MY MAC <mymac@MYs-MacBook-Air.local>
2025-03-06 04:20:41 +0530

    Initial commit of Elite Resume Employee Web App

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/employee', require('./routes/employee'));
+app.use('/api/admin', require('./routes/admin'));
+
+// Serve HTML files
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/login.html'));
+});
+
+app.get('/employee', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/employee.html'));
+});
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/admin.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
